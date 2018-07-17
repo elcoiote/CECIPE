6c6ee8d5027a4563cc6419dccc998d727576fe1c
Eduardo Lopes <EduardoLopes_br@hotmail.com>
2018-07-17 13:00:42 -0300

    Criação de object library

---
+++ CCPv1/CCP_Pessoa/CCP_ObjectLib.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CCP_Pessoa
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Representante { get; set; }
+        public string DataInicioAssitenciaLar { get; set; }
+        public string DataFimAssitenciaLar { get; set; }
+        public string HoraAssitenciaLar { get; set; }
+        public int NumeroSemanas { get; set; }
+
+        public void PrintMe()
+        {
+            Console.WriteLine("Nome: {0}", Nome);
+            Console.WriteLine("Representante: {0}", Representante);
+            Console.WriteLine("Assistência no Lar - período de {0} a {1} às {2}",
+                DataInicioAssitenciaLar, DataFimAssitenciaLar, HoraAssitenciaLar);
+            Console.WriteLine("Reuniões Públicas - Palestras e Passes  - durante {0} semanas",
+                NumeroSemanas);
+        }
+    }
+}
--- CCPv1/CCPv1/Program.cs
+++ CCPv1/CCPv1/Program.cs
@@ -1,42 +1,47 @@
-﻿using System;
+﻿using CCP_Pessoa;
+using System;
 using System.Collections.Generic;
 
 namespace CCPv1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             List<Pessoa> lstPessoas = new List<Pessoa>();
-
-            Pessoa p1 = new Pessoa();
-            p1.Nome = "Eduardo Silva de Azeredo Lopes";
-            p1.Representante = "-";
-            p1.DataInicioAssitenciaLar = "01/01/2018";
-            p1.DataFimAssitenciaLar = "01/04/2018";
-            p1.HoraAssitenciaLar = "02:20";
-            p1.NumeroSemanas = 12;
-
-            lstPessoas.Add(p1);
-            ShowLstPessoas(lstPessoas);
-            Console.ReadKey();
-            Console.Clear();
-
-            p1 = new Pessoa();
-            p1.Nome = "Karla Berninger de Azeredo Lopes";
-            p1.Representante = "-";
-            p1.DataInicioAssitenciaLar = "02/02/2019";
-            p1.DataFimAssitenciaLar = "02/02/2019";
-            p1.HoraAssitenciaLar = "03:30";
-            p1.NumeroSemanas = 10;
+            Pessoa p1 = new Pessoa
+            {
+                Nome = "Eduardo Silva de Azeredo Lopes",
+                Representante = "-",
+                DataInicioAssitenciaLar = "01/01/2018",
+                DataFimAssitenciaLar = "01/04/2018",
+                HoraAssitenciaLar = "02:20",
+                NumeroSemanas = 12
+            };
 
             lstPessoas.Add(p1);
             ShowLstPessoas(lstPessoas);
             Console.ReadKey();
             Console.Clear();
 
+            p1 = new Pessoa
+            {
+                Nome = "Karla Berninger de Azeredo Lopes",
+                Representante = "-",
+                DataInicioAssitenciaLar = "02/02/2019",
+                DataFimAssitenciaLar = "02/02/2019",
+                HoraAssitenciaLar = "03:30",
+                NumeroSemanas = 10
+            };
+
+            lstPessoas.Add(p1);
+            ShowLstPessoas(lstPessoas);
+            Console.ReadKey();
             Console.Clear();
-            ShowMenu();
+
+            Console.Clear();
+            ShowMenu(lstPessoas);
 
             Console.WriteLine("Fim do programa. Tecle algo para terminar.");
             Console.ReadKey();
@@ -51,7 +56,7 @@ namespace CCPv1
             }
         }
 
-        static void ShowMenu()
+        static void ShowMenu(List<Pessoa> lst)
         {
             int opcao = Convert.ToInt32('0');
 
@@ -86,25 +91,7 @@ namespace CCPv1
         }
     }
 
-    class Pessoa
-    {
-        public string Nome { get; set; }
-        public string Representante { get; set; }
-        public string DataInicioAssitenciaLar { get; set; }
-        public string DataFimAssitenciaLar { get; set; }
-        public string HoraAssitenciaLar { get; set; }
-        public int NumeroSemanas { get; set; }
-
-        public void PrintMe()
-        {
-            Console.WriteLine("Nome: {0}", Nome);
-            Console.WriteLine("Representante: {0}", Representante);
-            Console.WriteLine("Assistência no Lar - período de {0} a {1} às {2}",
-                DataInicioAssitenciaLar, DataFimAssitenciaLar, HoraAssitenciaLar);
-            Console.WriteLine("Reuniões Públicas - Palestras e Passes  - durante {0} semanas",
-                NumeroSemanas);
-        }
-    }
+    
 
   
 }
